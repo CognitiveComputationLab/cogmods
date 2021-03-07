8274447e50cd9216cfedf01b508f4cb14a31861e
monthie <42239824+monthie@users.noreply.github.com>
2021-03-07 19:21:33 +0100

    Add Readme

--- wason_analyse/PSYCOP.py
+++ wason_analyse/PSYCOP.py
@@ -11,19 +11,44 @@ def if_elimination_converse(card, q):
 def not_introduction(card, q):
     q[1] = not q[1]
     if if_elimination(card, q):
+        q[1] = not q[1]
         return True
+    else:
+        q[1] = not q[1]
+        return False
 
 
-def main():
+def main(c="biconditional", i="imagine"):
     p = ["vowel", True]  # vowel
     q = ["even", True]  # even
     cards = {1: ["vowel", True], 2: ["vowel", False], 3: ["even", True], 4: ["even", False]}
     count = 0
     for card in cards.values():
         count += 1
-        if if_elimination(card, p) or if_elimination_converse(card, q) or not_introduction(card, q):
-            print("Select card: ", count)
+        if c == "biconditional":
+            if i == "imagine":
+                if if_elimination(card, p):
+                    print("Select card: ", card)
+                if if_elimination_converse(card, q):
+                    print("Select card: ", card)
+                if not_introduction(card, q):
+                    print("Select card: ", card)
+            elif i == "noimagine":
+                if if_elimination(card, p):
+                    print("Select card: ", card)
+                if if_elimination_converse(card, q):
+                    print("Select card: ", card)
+        elif c == "conditional":
+            if i == "imagine":
+                if if_elimination(card, p):
+                    print("Select card: ", card)
+                if not_introduction(card, q):
+                    print("Select card: ", card)
+            elif i == "noimagine":
+                if if_elimination(card, p):
+                    print("Select card: ", card)
+
 
 
 if __name__ == "__main__":
-    main()
+    main(c="biconditional", i="imagine")
